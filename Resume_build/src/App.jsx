7c884cece4c7f406c67255b0eec51bf317ad95d6
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2024-09-27 12:47:27 +0530

    changes

--- Resume_build/src/App.jsx
+++ Resume_build/src/App.jsx
@@ -1,28 +1,43 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'; // If you still need this for other styles
 import ResumeForm from './components/ResumeForm1.jsx';
 import Resume from './components/resume1.jsx';
 import PrintButton from './components/PrintButton.jsx';
+import Home from './components/Pages/Home'; // Import Home component
 import data from './resumeData1.json';
 
 function App() {
   const [count, setCount] = useState(0);
-  const data = JSON.parse(localStorage.getItem("resumeFormData"));
+  const storedData = JSON.parse(localStorage.getItem("resumeFormData"));
 
   return (
-    <div className="App">
-      <div className="flex flex-row h-screen">
-        {/* Resume on the left */}
-        <div className="w-1/2 bg-gray-100 p-4">
-          <Resume resumeData={data} />
-        </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Home page route */}
+          <Route path="/" element={<Home />} />
+          
+          {/* Resume builder route */}
+          <Route 
+            path="/builder" 
+            element={
+              <div className="flex flex-row h-screen">
+                {/* Resume on the left */}
+                <div className="w-1/2 bg-gray-100 p-4">
+                  <Resume resumeData={storedData || data} />
+                </div>
 
-        {/* Form on the right, hidden during print */}
-        <div className="w-1/2 bg-white p-4 hide-on-print">
-          <ResumeForm />
-        </div>
+                {/* Form on the right, hidden during print */}
+                <div className="w-1/2 bg-white p-4 hide-on-print">
+                  <ResumeForm />
+                </div>
+              </div>
+            } 
+          />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
