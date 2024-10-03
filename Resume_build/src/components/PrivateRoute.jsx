34ec3837f82bfcca1988cbe37557927f01800a01
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2024-10-03 08:46:04 +0530

    private route changed

--- Resume_build/src/components/PrivateRoute.jsx
+++ Resume_build/src/components/PrivateRoute.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { Outlet, Navigate } from 'react-router-dom';
 
 export default function PrivateRoute() {
-  const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
+  const { user } = useSelector((state) => state.auth);
+  console.log(user,"private route")
+  return user ? <Outlet /> : <Navigate to='/sign-in' />;
 }
