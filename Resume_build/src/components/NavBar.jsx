f667e9bef40d950c35435315196ef551d27f304b
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-10-12 17:30:06 +0530

    made navbar sticky

--- Resume_build/src/components/NavBar.jsx
+++ Resume_build/src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-blue-600 p-4">
+    <nav className="bg-blue-600 p-4 sticky top-0 z-50 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-white text-xl font-bold">
           <Link to="/" className="hover:underline">Resume Builder</Link>
